75551fa2312c0d06854ce85cedcab6a01ea5171f
Maryia Vinogradova <maryiavinogradova@Maryias-MacBook-Air.local>
2022-01-30 22:39:16 -0800

    week5 test is added

---
+++ Develop/script.js
@@ -0,0 +1,59 @@
+
+var day = moment().format("MMM Do YY");
+$("#currentDay").append(day);
+
+$(document).ready(function () {
+  // saveBtn click listener 
+  $(".saveBtn").on("click", function () {
+      // Get nearby values of the description in JQuery
+      var text = $(this).siblings(".event").val();
+      var time = $(this).parent().attr("id");
+
+      // Save text in local storage
+      localStorage.setItem(time, text);
+  })
+ 
+  function timeTracker() {
+      //get current number of hours.
+      var presentTime = moment().hour();
+
+      // loop over time blocks
+      $(".time-block").each(function () {
+          var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+          // To check the time and add the classes for background indicators
+          if (blockTime < presentTime) {
+              $(this).removeClass("future");
+              $(this).removeClass("present");
+              $(this).addClass("past");
+          }
+          else if (blockTime === presentTime) {
+              $(this).removeClass("past");
+              $(this).removeClass("future");
+              $(this).addClass("present");
+          }
+          else {
+              $(this).removeClass("present");
+              $(this).removeClass("past");
+              $(this).addClass("future");
+
+          }
+      })
+  }
+
+  // Get item from local storage if any
+  $("#hour-7 .event").val(localStorage.getItem("hour-7"));
+  $("#hour-8 .event").val(localStorage.getItem("hour-8"));
+  $("#hour-9 .event").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .event").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .event").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .event").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .event").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .event").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .event").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .event").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .event").val(localStorage.getItem("hour-17"));
+  $("#hour-18 .event").val(localStorage.getItem("hour-18"));
+
+  timeTracker();
+})
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # week5
+# 05 Third-Party APIs: Work Day Scheduler
+
+## My work accord to my task
+
+According to our task we supposed to create day planner for every hour to create a day schedule. Every hour row has own background color past/present/future comparasing to my current time. Time needs to be prented by [Moment.js](https://momentjs.com/) library. Select elements from HTML file by jQuery selector. 
+``
+
+## Criterias and application
+
+My application needs to save my planned event per each hour to local storage and insulate it after refresh (or re-open) the page of application. I see every hour is shown in diffrent color acoording to previous statment (pas/present/future - these classes we had in provided CSS). My HTML file shows my bisness time when I will plan my day (it could be set up with inner HTML function to replace each div per hour from my HTML). When I click Save button that saves my input into local storeage and keep it in my day events "textareas" per hour
+
+
+###
+* The URL of the deployed application
+
+* The URL of the GitHub repository, with a unique name and a README describing the project
+
+
+
+####
+P.S. Sorry if my description is not so elliquent - that is only because it is so hard for me to describe it in English. =)) 
