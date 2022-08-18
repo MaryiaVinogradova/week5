1f7ffd4b28eac6dbee2c0477af2319c70828b145
Maryia Vinogradova <maryiavinogradova@Maryias-MacBook-Air.local>
2022-08-17 23:58:16 -0600

    Code has changed

--- script.js
+++ script.js
@@ -1,79 +1,57 @@
-// var day = moment().format("MMM Do YY");
-// $("#currentDay").append(day);
+//start function
+$(document).ready(function(){
+    // setting currentDay event using moments
+    var today = moment().format("dddd, MMMM Do, YYYY, h:mm a");
+    $("#currentDay").append(today);
+    console.log(today);
+    
+    var now = parseInt(moment().format("H"));
+    
+    // setting storage events 
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".event").val();
+        var time = $(this).parent().attr("id");
+  
+        localStorage.setItem(time, text);
+    })
+  
+      // Get item from local storage to show up your events per hour
+      $("#hr7 .event").val(localStorage.getItem("hr7"));
+      $("#hr8 .event").val(localStorage.getItem("hr8"));
+      $("#hr9 .event").val(localStorage.getItem("hr9"));
+      $("#hr10 .event").val(localStorage.getItem("hr10"));
+      $("#hr11 .event").val(localStorage.getItem("hr11"));
+      $("#hr12 .event").val(localStorage.getItem("hr12"));
+      $("#hr13 .event").val(localStorage.getItem("hr13"));
+      $("#hr14 .event").val(localStorage.getItem("hr14"));
+      $("#hr15 .event").val(localStorage.getItem("hr15"));
+      $("#hr16 .event").val(localStorage.getItem("hr16"));
+      $("#hr17 .event").val(localStorage.getItem("hr17"));
+      $("#hr18 .event").val(localStorage.getItem("hr18"));
+    
 
-$(document).ready(function () {
-
-  $(".saveBtn").on("click", function () {
-      var text = $(this).siblings(".event").val();
-      var time = $(this).parent().attr("id");
-
-      localStorage.setItem(time, text);
-  })
- 
-  function timeTracker() {
-      var presentTime = moment().hour();
-
-      var day = moment().format("MMM Do YY");
-      $("#currentDay").append(day);
-
-      $(".time-block-7") = (function () {
-          var hr7 = $('#hour-7');
-          var hr8 = $('#hour-8');
-          var hr9 = $('#hour-9');
-          var hr10 = $('#hour-10');
-
-          var dayTime = ["7", "8", "9", "10"]
-
-          function updateColor(){
-            for(var i=0; i<dayTime.length; i++){
-                if(parseInt(dayTime[i])>day){
-                    $("#"+dayTime[i]).addClass("future")
-                }
-                else if(parseInt(dayTime[i])<day){
-                    $("#"+dayTime[i].addClass("past"))
-                }
-                else if(parseInt(dayTime[i])==day){
-                    $("#"+day[i]).addClass("present")
-                }
-            }
-            
-          }
-          updateColor();
-    //       var hr7 = parseInt($('.time-block-7').attr("id").split("hour")[1]);
-
-    //       if (hr7 < presentTime) {
-    //           $(this).removeClass("future");
-    //           $(this).removeClass("present");
-    //           $(this).addClass("past");
-    //       }
-    //       else if (hr7 === presentTime) {
-    //           $(this).removeClass("past");
-    //           $(this).removeClass("future");
-    //           $(this).addClass("present");
-    //       }
-    //       else {
-    //           $(this).removeClass("present");
-    //           $(this).removeClass("past");
-    //           $(this).addClass("future");
-
-    //       }
-    //   });
-      
-  }
-
-  // Get item from local storage to show up your events per hour
-  $("#hour-7 .event").val(localStorage.getItem("hour-7"));
-  $("#hour-8 .event").val(localStorage.getItem("hour-8"));
-  $("#hour-9 .event").val(localStorage.getItem("hour-9"));
-  $("#hour-10 .event").val(localStorage.getItem("hour-10"));
-  $("#hour-11 .event").val(localStorage.getItem("hour-11"));
-  $("#hour-12 .event").val(localStorage.getItem("hour-12"));
-  $("#hour-13 .event").val(localStorage.getItem("hour-13"));
-  $("#hour-14 .event").val(localStorage.getItem("hour-14"));
-  $("#hour-15 .event").val(localStorage.getItem("hour-15"));
-  $("#hour-16 .event").val(localStorage.getItem("hour-16"));
-  $("#hour-17 .event").val(localStorage.getItem("hour-17"));
-  $("#hour-18 .event").val(localStorage.getItem("hour-18"));
-
-  timeTracker();
-})
+    // setting hour colors
+    var dayTime = ["23", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18"]
+    updateschedule();
+    
+    function updateschedule() {
+      var Time = moment().format('H');
+      for(var i = 0; i < dayTime.length; i++) {
+    
+        if (parseInt(dayTime[i]) > Time) {
+          $("#" + dayTime[i]).addClass("future");
+    
+        }
+        else if (parseInt(dayTime[i]) < Time) {
+          $("#" + dayTime[i]).addClass("past");
+    
+        }
+        else if (parseInt(dayTime[i]) == Time) {
+          $("#" + dayTime[i]).addClass("present");
+        
+        }
+      }
+    }
+    
+    });
+    
